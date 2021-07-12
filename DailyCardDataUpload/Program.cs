04a01c48840d3418d99457dcaa5e96f06efe6c59
KINGTAEHEE <thk919@gmail.com>
2021-07-12 17:41:06 +0900

    Update Program.cs
    인수 2개로 특정 기간 동안의 데이터를 처리할 수 있도록 기능 추가

--- DailyCardDataUpload/Program.cs
+++ DailyCardDataUpload/Program.cs
@@ -16,7 +16,9 @@ namespace DailyCardDataUpload
         static RfcDestination m_rfcDestination = null;
         static RfcRepository m_rfcRepository = null;
         static IRfcFunction m_rfcFunction;
-		static string targetDate = String.Empty;
+		static string targetDate = string.Empty;
+        static string targetDateStart = string.Empty;
+        static string targetDateEnd = string.Empty;
 		static DataTable dtCard = new DataTable();
 		static DataTable dtKakao = new DataTable();
 		static DataTable dtZero = new DataTable();
@@ -38,7 +40,7 @@ namespace DailyCardDataUpload
 				WriteLog("RFC 결과 : " + l_strErrorCode + "\t" + l_strErrorMessage);
 				DeleteFile();
 			}
-			else // 수동 (날짜 인수 있을 때)
+			else if (args.Length == 1) // 수동 (날짜 인수 1개 있을 때 : 특정 날짜만)
 			{
 				targetDate = args[0].ToString();
 				FtpDownloadUsingWebClient();
@@ -53,6 +55,29 @@ namespace DailyCardDataUpload
 				WriteLog("RFC 결과 : " + l_strErrorCode + "\t" + l_strErrorMessage);
 				DeleteFile();
 			}
+            else if (args.Length == 2) // 수동 (날짜 인수 2개 있을 때 : 특정 기간동안)
+            {
+                targetDateStart = args[0].ToString();
+                targetDateEnd = args[1].ToString();
+                targetDate = targetDateStart;
+                int count = (DateTime.ParseExact(targetDateEnd, "yyyyMMdd", null) - DateTime.ParseExact(targetDateStart, "yyyyMMdd", null)).Days;
+                for (int i = 0; i <= count; i++)
+                {
+                    FtpDownloadUsingWebClient();
+                    ParsingData();
+                    RFCInfoSet("[SD] KICC 카드입금정보 및 수수료 정보", "*");
+                    InsertRfcDataImport("I_ZECDAT", targetDate);
+                    string l_strTableName = "IT_SALES";
+                    InsertRfcDataRows("*", l_strTableName, dtCard);
+                    RfcFunctionCall();
+                    string l_strErrorCode = m_rfcFunction.GetValue("E_RESULT").ToString();
+                    string l_strErrorMessage = m_rfcFunction.GetValue("E_MESSAGE").ToString();
+                    WriteLog("RFC 결과 : " + l_strErrorCode + "\t" + l_strErrorMessage);
+                    DeleteFile();
+                    dtCard = new DataTable(); // 작업 완료 후 초기화
+                    targetDate = DateTime.ParseExact(targetDate, "yyyyMMdd", null).AddDays(1).ToString("yyyyMMdd");
+                }
+            }
         }
 
 		public static void FtpDownloadUsingWebClient()
@@ -123,140 +148,137 @@ namespace DailyCardDataUpload
 
 		public static void ParsingData()
 		{
-			// 카드 데이터
-			using (StreamReader sr = new StreamReader("YPBOOKS001_REP." + targetDate, Encoding.Default))
-			{
-                DataColumn DR01 = new DataColumn("DR01", typeof(string));
-                DataColumn DR02 = new DataColumn("DR02", typeof(string));
-                DataColumn DR03 = new DataColumn("DR03", typeof(string));
-                DataColumn DR04 = new DataColumn("DR04", typeof(string));
-                DataColumn DR05 = new DataColumn("DR05", typeof(string));
-                DataColumn DR06 = new DataColumn("DR06", typeof(string));
-                DataColumn DR07 = new DataColumn("DR07", typeof(string));
-                DataColumn DR08 = new DataColumn("DR08", typeof(string));
-                DataColumn DR09 = new DataColumn("DR09", typeof(string));
-                DataColumn DR10 = new DataColumn("DR10", typeof(string));
-                DataColumn DR11 = new DataColumn("DR11", typeof(string));
-                DataColumn DR12 = new DataColumn("DR12", typeof(string));
-                DataColumn DR13 = new DataColumn("DR13", typeof(string));
-                DataColumn DR14 = new DataColumn("DR14", typeof(string));
-                DataColumn DR15 = new DataColumn("DR15", typeof(string));
-                DataColumn DR16 = new DataColumn("DR16", typeof(string));
-                DataColumn DR17 = new DataColumn("DR17", typeof(string));
-                DataColumn HR01 = new DataColumn("HR01", typeof(string));
-                DataColumn HR02 = new DataColumn("HR02", typeof(string));
-                DataColumn HR03 = new DataColumn("HR03", typeof(string));
-                DataColumn HR04 = new DataColumn("HR04", typeof(string));
-                DataColumn HR05 = new DataColumn("HR05", typeof(string));
-                dtCard.Columns.Add(DR01);
-                dtCard.Columns.Add(DR02);
-                dtCard.Columns.Add(DR03);
-                dtCard.Columns.Add(DR04);
-                dtCard.Columns.Add(DR05);
-                dtCard.Columns.Add(DR06);
-                dtCard.Columns.Add(DR07);
-                dtCard.Columns.Add(DR08);
-                dtCard.Columns.Add(DR09);
-                dtCard.Columns.Add(DR10);
-                dtCard.Columns.Add(DR11);
-                dtCard.Columns.Add(DR12);
-                dtCard.Columns.Add(DR13);
-                dtCard.Columns.Add(DR14);
-                dtCard.Columns.Add(DR15);
-                dtCard.Columns.Add(DR16);
-                dtCard.Columns.Add(DR17);
-                dtCard.Columns.Add(HR01);
-                dtCard.Columns.Add(HR02);
-                dtCard.Columns.Add(HR03);
-                dtCard.Columns.Add(HR04);
-                dtCard.Columns.Add(HR05);
+            try
+            {
+                // 카드 데이터
+                using (StreamReader sr = new StreamReader("YPBOOKS001_REP." + targetDate, Encoding.Default))
+                {
+                    dtCard.Columns.Add(new DataColumn("DR01", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR02", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR03", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR04", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR05", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR06", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR07", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR08", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR09", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR10", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR11", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR12", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR13", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR14", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR15", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR16", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("DR17", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("HR01", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("HR02", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("HR03", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("HR04", typeof(string)));
+                    dtCard.Columns.Add(new DataColumn("HR05", typeof(string)));
+                    string[] dr = new string[22];
+                    string tmp = string.Empty;
 
-                string[] dr = new string[22];
+                    while (true)
+                    {
+                        tmp = sr.ReadLine();
 
-                string tmp = String.Empty;
+                        // START RECORD
+                        if (tmp.Substring(0, 2).Equals("51"))
+                        {
+                            continue;
+                        }
 
-				while (true)
-				{
-					tmp = sr.ReadLine();
+                        // HEADER RECORD
+                        if (tmp.Substring(0, 2).Equals("52"))
+                        {
+                            dr[17] = tmp.Substring(18, 10); // 가맹점 사업자등록번호
+                            dr[18] = tmp.Substring(40, 6);  // 지급일자
+                            dr[19] = tmp.Substring(46, 15); // 가맹점번호
+                            dr[20] = tmp.Substring(79, 6);  // 청구일자
+                            dr[21] = tmp.Substring(85, 4);  // 카드사 코드
+                            continue;
+                        }
 
-					// START RECORD
-					if (tmp.Substring(0, 2).Equals("51"))
-					{
-						continue;
-					}
+                        // DATA RECORD
+                        /*
+                         * 60 : 매출정상		61 : 매출반송		62 : 매출보류
+                         * 63 : 매출보류해제	64 : 취소반송		65 : 취소보류
+                         * 66 : 취소보류해제	67 : 취소매출정상
+                         */
+                        if (tmp.Substring(0, 2).Equals("60") || tmp.Substring(0, 2).Equals("61") || tmp.Substring(0, 2).Equals("62") ||
+                            tmp.Substring(0, 2).Equals("63") || tmp.Substring(0, 2).Equals("64") || tmp.Substring(0, 2).Equals("65") ||
+                            tmp.Substring(0, 2).Equals("66") || tmp.Substring(0, 2).Equals("67"))
+                        {
+                            dr[0] = tmp.Substring(0, 2);    // RECORD 구분
+                            dr[1] = tmp.Substring(2, 3);    // 통화코드
+                            dr[2] = tmp.Substring(5, 1);    // 통화지수
+                            dr[3] = tmp.Substring(6, 6);    // 매출(취소)일자
+                            dr[4] = tmp.Substring(12, 6);   // 접수일자
+                            dr[5] = tmp.Substring(18, 19);  // 카드번호
+                            dr[6] = tmp.Substring(37, 2);   // 할부기간
+                            dr[7] = tmp.Substring(39, 10);  // 신용판매금액
+                            dr[8] = tmp.Substring(49, 4);   // 반송사유 코드(카드사 반송코드)
+                            dr[9] = tmp.Substring(53, 2);   // 반송사유 코드(VAN사 관리 표준 반송코드)
+                            dr[10] = tmp.Substring(55, 13); // 주민등록번호
+                            dr[11] = tmp.Substring(68, 10); // 수수료금액
 
-					// HEADER RECORD
-					if (tmp.Substring(0, 2).Equals("52"))
-					{
-						dr[17] = tmp.Substring(18, 10); // 가맹점 사업자등록번호
-                        dr[18] = tmp.Substring(40, 6); // 지급일자
-                        dr[19] = tmp.Substring(46, 15); // 가맹점번호
-                        dr[20] = tmp.Substring(79, 6); // 청구일자
-                        dr[21] = tmp.Substring(85, 4); // 카드사 코드
-						continue;
-					}
+                            // Substring 한글 자릿수 문제 해결
+                            int count = 0;
+                            char[] values = tmp.Substring(78, 40).ToCharArray();
+                            foreach (char c in values)
+                            {
+                                int value = Convert.ToInt32(c);
+                                if (value >= 0x80)
+                                {
+                                    count++;
+                                }
+                            }
 
-					// DATA RECORD
-					if (tmp.Substring(0, 2).Equals("60") || tmp.Substring(0, 2).Equals("61") || tmp.Substring(0, 2).Equals("62") ||
-						tmp.Substring(0, 2).Equals("63") || tmp.Substring(0, 2).Equals("64") || tmp.Substring(0, 2).Equals("65") ||
-						tmp.Substring(0, 2).Equals("66") || tmp.Substring(0, 2).Equals("67"))
-					{
-						/*
-						 * 60 : 매출정상		61 : 매출반송		62 : 매출보류
-						 * 63 : 매출보류해제	64 : 취소반송		65 : 취소보류
-						 * 66 : 취소보류해제	67 : 취소매출정상
-						 */
+                            dr[12] = tmp.Substring(78, 20 - count);  // 반송사유
+                            dr[13] = tmp.Substring(98 - count, 20);  // 거래고유번호
+                            dr[14] = tmp.Substring(118 - count, 20); // 단말기번호
+                            dr[15] = tmp.Substring(138 - count, 10); // 승인번호
+                            dr[16] = tmp.Substring(148 - count, 2);  // FILLER
 
-						dr[0] = tmp.Substring(0, 2); // RECORD 구분
-                        dr[1] = tmp.Substring(2, 3); // 통화코드
-                        dr[2] = tmp.Substring(5, 1); // 통화지수
-                        dr[3] = tmp.Substring(6, 6); // 매출(취소)일자
-                        dr[4] = tmp.Substring(12, 6); // 접수일자
-                        dr[5] = tmp.Substring(18, 19); // 카드번호
-                        dr[6] = tmp.Substring(37, 2); // 할부기간
-                        dr[7] = tmp.Substring(39, 10); // 신용판매금액
-                        dr[8] = tmp.Substring(49, 4); // 반송사유 코드(카드사 반송코드)
-                        dr[9] = tmp.Substring(53, 2); // 반송사유 코드(VAN사 관리 표준 반송코드)
-                        dr[10] = tmp.Substring(55, 13); // 주민등록번호
-                        dr[11] = tmp.Substring(68, 10); // 수수료금액
-						// Substring 한글 자릿수 문제 해결
-						int count = 0;
-						char[] values = tmp.Substring(78, 40).ToCharArray();
-						foreach (char c in values)
-						{
-							int value = Convert.ToInt32(c);
-							if (value >= 0x80)
-							{
-								count++;
-							}
-						}
-                        dr[12] = tmp.Substring(78, 20 - count); // 반송사유
-                        dr[13] = tmp.Substring(98 - count, 20); // 거래고유번호
-                        dr[14] = tmp.Substring(118 - count, 20); // 단말기번호
-                        dr[15] = tmp.Substring(138 - count, 10); // 승인번호
-                        dr[16] = tmp.Substring(148 - count, 2); // FILLER
+                            // DataRow 추가
+                            dtCard.Rows.Add(dr);
+                            continue;
+                        }
 
-                        // DataRow 추가
-                        DataRow row = dtCard.NewRow();
-                        row.ItemArray = dr;
-                        dtCard.Rows.Add(row);
-                        continue;
-					}
+                        // TOTAL RECORD
+                        if (tmp.Substring(0, 2).Equals("53"))
+                        {
+                            continue;
+                        }
 
-					// TOTAL RECORD
-					if (tmp.Substring(0, 2).Equals("53"))
-					{
-						continue;
-					}
+                        // EDI-END RECORD
+                        if (tmp.Substring(0, 2).Equals("54"))
+                        {
+                            break;
+                        }
+                    }
+                }
 
-					// EDI-END RECORD
-					if (tmp.Substring(0, 2).Equals("54"))
-					{
-						break;
-					}
-				}
-			}
-		}
+                /*
+                // 카카오 데이터
+                using (StreamReader sr = new StreamReader("YPBOOKS001_KAKAO_REP." + targetDate, Encoding.Default))
+                {
+
+                }
+
+                // 제로페이 데이터
+                using (StreamReader sr = new StreamReader("YPBOOKS001_ZPP." + targetDate, Encoding.Default))
+                {
+
+                }
+                */
+            }
+            catch (Exception ex)
+            {
+                WriteLog(ex.Message);
+                dtCard = new DataTable(); // 오류 발생시 초기화
+            }
+        }
 
 		public static bool RFCInfoSet(string _rfcName, string _rfcFunction)
         {
@@ -264,12 +286,12 @@ namespace DailyCardDataUpload
             {
                 // RFC 정보 셋팅
                 RfcConfigParameters rfc = new RfcConfigParameters();
-                rfc[RfcConfigParameters.Name] = "YPP";
+                rfc[RfcConfigParameters.Name] = "*";
                 rfc[RfcConfigParameters.PoolSize] = Convert.ToString(190);
                 rfc[RfcConfigParameters.PeakConnectionsLimit] = Convert.ToString(200);
                 rfc[RfcConfigParameters.MaxPoolWaitTime] = Convert.ToString(1);
-                rfc[RfcConfigParameters.Client] = "*"; // 개발
-                rfc[RfcConfigParameters.AppServerHost] = "*";
+                rfc[RfcConfigParameters.Client] = "*";
+                rfc[RfcConfigParameters.AppServerHost] = "*.*.*.*";
 				rfc[RfcConfigParameters.User] = "*";
 				rfc[RfcConfigParameters.Password] = "*";
 				rfc[RfcConfigParameters.Language] = "KO";
